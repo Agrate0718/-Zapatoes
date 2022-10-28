49738641288b6ed4a43d2bf19bd1bbfdb01edaec
Agrate071818 <zionagrate@gmail.com>
2022-10-27 22:45:53 -0400

    All basic crud completed

---
+++ zapatoes-client/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <header>
+        {/* < /> */}
+      </header>
+         <div className="App">
+     <p>apple</p>
+    <Routes>
+      
+    </Routes>
+      </div>
+    </Router>
+   
+  );
+}
+
+export default App;

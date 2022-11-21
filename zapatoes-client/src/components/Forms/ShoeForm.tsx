3229cd1379e255361d62f91e75849045b6e2780b
Agrate071818 <zionagrate@gmail.com>
2022-11-20 22:58:37 -0500

    added to shoe card form

--- zapatoes-client/src/components/Forms/ShoeForm.tsx
+++ zapatoes-client/src/components/Forms/ShoeForm.tsx
@@ -82,15 +82,18 @@ export default function ShoeForm(){
                         />
                         
                     </div>
-                    
-                    <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-                    <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    {/* "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" */}
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="price">Price:</label>
+                    {/* <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label> */}
+                    <select id="countries" className= "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                     <option selected>Choose a brand</option>
-                    <option value="US">United States</option>
-                    <option value="CA">Canada</option>
-                    <option value="FR">France</option>
-                    <option value="DE">Germany</option>
+                    {/* <option value={goes here like this}> */}
+                    <option>Nike</option> 
+                    <option>Addidas</option>
+                    <option>Jordan</option>
+                    <option>FILA</option>
                     </select>
+                    {/* more stuff */}
 
                     <button className='bg-[#111827] hover:bg-[#7188b8] text-white uppercase rounded ' type='submit'>Submit</button>
                     <button className="inline-flex items-center ml-20 mt-10 uppercase text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-bred-600 dark:hover:bg-red-700 dark:focus:ring-red-800" onClick={() => navigate(-1)}>cancel</button>

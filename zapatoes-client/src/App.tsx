de88cd39c1f9402584b1f172a7688ba9dca5a93e
Agrate0718 <zionagrate@gmail.com>
2023-02-09 23:59:58 -0500

    .

--- zapatoes-client/src/App.tsx
+++ zapatoes-client/src/App.tsx
@@ -1,120 +1,100 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import ShoeEditForm from './components/Forms/ShoeEditForm';
-import Details from './components/pages/Details';
-import Home from './components/pages/Home';
-import Inquiries from './components/pages/Inquiries';
-import TestPage from './components/pages/TestPage';
-import Navbar from './components/partials/navbar';
-import Shoes from './components/partials/Shoes';
-import ShoeDetails from './components/partials/ShoeDetails';
-import jwt_decode from 'jwt-decode'
-import { useEffect, useState } from 'react';
-import Login from './components/Forms/Login';
-import axios from 'axios';
-import ShoeForm from './components/Forms/ShoeForm';
-import InquiryForm from './components/Forms/InquiryForm';
-import InquiryDetails from './components/partials/InquiryDetails';
-import OwnerShoes from './components/partials/OwnerShoes';
-import OwnerShoeDetails from './OwnerShoeDetails';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ShoeEditForm from "./components/Forms/ShoeEditForm";
+import Details from "./components/pages/Details";
+import Home from "./components/pages/Home";
+import Inquiries from "./components/pages/Inquiries";
+import TestPage from "./components/pages/TestPage";
+import Navbar from "./components/partials/navbar";
+import Shoes from "./components/partials/Shoes";
+import ShoeDetails from "./components/partials/ShoeDetails";
+import jwt_decode from "jwt-decode";
+import { useEffect, useState } from "react";
+import Login from "./components/Forms/Login";
+import axios from "axios";
+import ShoeForm from "./components/Forms/ShoeForm";
+import InquiryForm from "./components/Forms/InquiryForm";
+import InquiryDetails from "./components/partials/InquiryDetails";
+import OwnerShoes from "./components/partials/OwnerShoes";
+import OwnerShoeDetails from "./OwnerShoeDetails";
 // import Misc from './components/Misc';
-
 
 function App() {
   // State
-  const [currentUser, setCurrentUser] = useState(null)
+  const [currentUser, setCurrentUser] = useState(null);
 
- // Hooks
+  // Hooks
   useEffect(() => {
     // check to see if token is in storage
-    const token = localStorage.getItem('jwt')
+    const token = localStorage.getItem("jwt");
     if (token) {
       // if so, we will decode it and set the user in app state
-      setCurrentUser(jwt_decode(token))
+      setCurrentUser(jwt_decode(token));
     } else {
-      setCurrentUser(null)
+      setCurrentUser(null);
     }
-  }, []) // happen only once
-
- 
+  }, []); // happen only once
 
   return (
     <Router>
-         <div>
-    <Routes>
-    <Route // Landing
+      <div>
+        <Routes>
+          <Route // Landing
             path="/"
             element={<Home />}
             // element={<Misc />}
           />
-        
-        <Route // Landing
+
+          <Route // Landing
             path="/test"
             element={<TestPage />}
           />
 
-        <Route // Edit shoe
-            path="/shoes/:shoeId/edit" 
-            element={<ShoeEditForm  />}
+          <Route // Edit shoe
+            path="/shoes/:shoeId/edit"
+            element={<ShoeEditForm />}
           />
 
-    <Route // Landing
+          <Route // Landing
             path="/Details"
             element={<Details />}
           />
 
-    <Route // Landing
+          <Route // Landing
             path="/Inquiry"
             element={<InquiryForm />}
           />
 
-<Route // Landing
+          <Route // Landing
             path="/Inquiries"
             element={<Inquiries />}
           />
 
-      <Route // Landing
+          <Route // Landing
             path="/View"
             element={<Shoes />}
           />
 
-        <Route
-          path='/shoes/get/:shoeId'
-          element={<ShoeDetails />}
-          />
-        <Route
-          path='/shoes/get/:shoeId/owner'
-          element={<OwnerShoeDetails />}
+          <Route path="/shoes/get/:shoeId" element={<ShoeDetails />} />
+          <Route
+            path="/shoes/get/:shoeId/owner"
+            element={<OwnerShoeDetails />}
           />
 
-        <Route
-          path='/owner'
-          element={<Login />}
-        />
+          <Route path="/owner" element={<Login />} />
 
-        <Route
-          path='/Create'
-          element={<ShoeForm />}
-        />
+          <Route path="/Create" element={<ShoeForm />} />
 
-        <Route
-          path='/Edit/:shoeId'
-          element={<ShoeEditForm />}
-        />
+          <Route path="/Edit/:shoeId" element={<ShoeEditForm />} />
 
-        <Route
-          path='/inquiries/get/:inquiryId'
-          element={<InquiryDetails />}
+          <Route
+            path="/inquiries/get/:inquiryId"
+            element={<InquiryDetails />}
           />
 
-        <Route
-          path='/owner/View'
-          element={<OwnerShoes />}
-        />
-
-    </Routes>
+          <Route path="/owner/View" element={<OwnerShoes />} />
+        </Routes>
       </div>
     </Router>
-   
   );
 }
 
--- zapatoes-client/src/components/partials/Shoes.tsx
+++ zapatoes-client/src/components/partials/Shoes.tsx
@@ -1,56 +1,58 @@
 import { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 import ShoeList from "../ShoeList";
 import Navbar from "./navbar";
 
+export default function Shoes() {
+  const [shoes, setShoes] = useState([]);
+  const [search, setSearch] = useState("");
 
-export default function Shoes(){
-   const [shoes, setShoes] = useState([])
-   const [search, setSearch] = useState("");
-   
-    useEffect(() => {
-        const getShoes = async () => {
-        
-            try{
-            const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/shoes/get`)
-            const filteredShoes = response.data.shoes.filter((shoe:any) => {
-                return shoe.name.toLowerCase().includes(search.toLowerCase());
-            });
+  useEffect(() => {
+    const getShoes = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_SERVER_URL}/shoes/get`
+        );
+        console.log(response);
+        const filteredShoes = response.data.shoes.filter((shoe: any) => {
+          return shoe.name.toLowerCase().includes(search.toLowerCase());
+        });
 
-            setShoes(filteredShoes);
-            // console.log(response.data)
-           
-        }catch(err) {
-            console.warn(err)
-           
-        }
-        }
-        getShoes()
-    }, [search])
+        setShoes(filteredShoes);
+        // console.log(response.data)
+      } catch (err) {
+        console.warn(err);
+      }
+    };
+    getShoes();
+  }, [search]);
 
-    console.log(shoes)
-    const shoeListComponents = shoes.map((shoe:any) => {
-        return <ShoeList shoe={shoe} key={shoe._id} />
-    })
-    return(
-        <div>
-            <header className='text-center'>
+  //   console.log(shoes);
+  const shoeListComponents = shoes.map((shoe: any) => {
+    return <ShoeList shoe={shoe} key={shoe._id} />;
+  });
+  return (
+    <div>
+      <header className="text-center">
         <Navbar />
       </header>
-          <div className="p-10">
-            <h1>All Shoes</h1>
-            {/* <ShoeSearch search={search} setSearch={setSearch} /> */}
-            <div>
-            <input type="search" value={search}
-                onChange={e => setSearch(e.target.value)}
-                className="w-full px-4 py-2 text-sm text-gray-700 bg-white border rounded-full shadow-sm overflow-visible focus:text-gray-700 focus:border-blue-600 focus:outline-none" placeholder="Search for some Shoes" aria-label="Search" 
-            />
-             </div>
-             <div className='mt-3 flex flex-wrap justify-center gap-8'>
-             {shoeListComponents}
-            </div>
-        </div>  
+      <div className="p-10">
+        <h1>All Shoes</h1>
+        {/* <ShoeSearch search={search} setSearch={setSearch} /> */}
+        <div>
+          <input
+            type="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="w-full px-4 py-2 text-sm text-gray-700 bg-white border rounded-full shadow-sm overflow-visible focus:text-gray-700 focus:border-blue-600 focus:outline-none"
+            placeholder="Search for some Shoes"
+            aria-label="Search"
+          />
         </div>
-        
-    )
+        <div className="mt-3 flex flex-wrap justify-center gap-8">
+          {shoeListComponents}
+        </div>
+      </div>
+    </div>
+  );
 }
